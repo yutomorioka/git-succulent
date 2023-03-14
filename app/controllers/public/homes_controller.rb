052fc0bebb6f97871377e6c2ea299d26a6eaee15
EC2 Default User <ec2-user@ip-172-31-36-3.ap-northeast-1.compute.internal>
2023-03-14 02:14:18 +0000

    Initial Commit

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -3,49 +3,8 @@ class Public::HomesController < ApplicationController
   end
 
   def search
-    @model = params['search']['model']
-    @content = params['search']['content']
-    @method = params['search']['method']
-    @result = search_for(@model, @content, @method)
+    @content = params[:content]
+    @records = Plant.search_for(@content)
   end
 
-  private
-
-   def search_for(model, content, method)
-    if model == 'user'
-      if method == 'forward'
-        User.where(
-          'name LIKE ? ',
-          "#{content}%"
-        )
-      elsif method == 'backward'
-        User.where(
-          'name LIKE ?',
-          "%#{content}"
-        )
-      elsif method == 'perfect'
-        User.where(
-          'name = ? ',
-          content
-        )
-      else # partial
-        User.where(
-          'last_name LIKE ?',
-          "%#{content}%"
-        )
-      end
-    elsif model == 'item'
-      if method == 'forward'
-        Plant.where('name LIKE ?', content + '%').includes(:genre)
-      elsif method == 'backward'
-        Plant.where('name LIKE ?', '%' + content).includes(:genre)
-      elsif method == 'perfect'
-        Plant.where(name: content).includes(:genre)
-      else # partial
-        Plant.where('name LIKE ?', '%' + content + '%').includes(:genre)
-      end
-    else
-      [] # 空配列を返す
-    end
-   end
 end
--- app/models/plant.rb
+++ app/models/plant.rb
@@ -2,4 +2,8 @@ class Plant < ApplicationRecord
 
    has_many :post_plants, dependent: :destroy
 
+   def self.search_for(content)
+     Plant.where('name LIKE ?', '%'+content+'%')
+   end
+
 end
