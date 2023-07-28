4474b5fefff4574b4c65f1fd0ca2290a43f0e85f
EC2 Default User <ec2-user@ip-172-31-36-3.ap-northeast-1.compute.internal>
2023-07-28 05:34:11 +0000

    Initial Commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,27 +3,27 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   validates :name, length: { in: 1..20 }, uniqueness: true
-  
+
   has_many :post_plants, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :comments, dependent: :destroy
-  
-  has_one_attached :image
-  
+
+  has_one_attached :profile_image
+
   def get_profile_image(width, height)
-    unless image.attached?
+    unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image.variant(resize_to_limit: [width, height]).processed
+      profile_image.variant(resize_to_limit: [width, height]).processed
   end
-  
+
   def self.guest
     find_or_create_by!(name: 'ゲストユーザー',  email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
     end
   end
-  
+
 end
